98a158d38e6ce404ca69a46d7ec20a51f8351eca
Jon Lamb <jon@auxon.io>
2023-12-12 09:17:10 -0800

    Add more sim info fields

--- fsw/src/sim_info.rs
+++ fsw/src/sim_info.rs
@@ -1,4 +1,5 @@
 use crate::units::{Time, Timestamp};
+use std::time::{Duration, Instant};
 
 #[derive(Debug, Clone)]
 pub struct SimulationInfo {
@@ -6,6 +7,8 @@ pub struct SimulationInfo {
     pub sim_iteration: u64,
     pub fsw_iteration: u64,
     pub relative_time: Time,
+    pub real_time_start: Instant,
+    pub real_time: Duration,
 }
 
 impl Default for SimulationInfo {
@@ -21,16 +24,20 @@ impl SimulationInfo {
             sim_iteration: 0,
             fsw_iteration: 0,
             relative_time: Time::from_secs(0.0),
+            real_time_start: Instant::now(),
+            real_time: Duration::ZERO,
         }
     }
 
     pub fn sim_step(&mut self, timestamp: Timestamp) {
         self.sim_iteration += 1;
         self.timestamp = timestamp;
+        self.real_time = Instant::now().duration_since(self.real_time_start);
     }
 
     pub fn fsw_step(&mut self, dt: Time) {
         self.fsw_iteration += 1;
         self.relative_time += dt;
+        self.real_time = Instant::now().duration_since(self.real_time_start);
     }
 }
